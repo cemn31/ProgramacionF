c5a2e33f1582a4c0a10eff877965b52a686f2c0b
cemn31 <31288486+cemn31@users.noreply.github.com>
2017-10-30 13:03:40 -0700

    Add files via upload

---
+++ Actividad4/Evaluacion1/esfera.f90
@@ -0,0 +1,65 @@
+!
+!! esfera.f90
+!! 
+!! Made by (Carlos Eduardo Martinez Nuñez)
+!! Login   <carloseduardo@ltsp142.example.com>
+!! 
+!! Started on  Mon Oct 30 11:09:27 2017 Carlos Eduardo Martinez Nuñez
+!! Last update Time-stamp: <30-oct-2017 11:32:02 carloseduardo>
+!
+
+ 
+
+program sphere
+
+! Calculate the surface area of a cylinder.
+!
+! Declare variables and constants.
+! constants=pi
+! variables=radius squared and height
+
+  implicit none    ! Require all variables to be explicitly declared
+
+  integer :: ierr
+  character(1) :: yn
+  real :: radius, area, volume
+  real, parameter :: pi = 3.141592653589793
+
+  interactive_loop: do
+
+!   Prompt the user for radius and height
+!   and read them.
+    write (*,*) 'FORTRAN APLICATION TO CALCULATE AREA AND VOLUME TO ONE SPHERE'
+    write (*,*) 'Enter radius of the sphere in cm.'
+    read (*,*,iostat=ierr) radius
+
+!   If radius and height could not be read from input,
+!   then cycle through the loop.
+
+    if (ierr /= 0) then
+      write(*,*) 'Error, invalid input.'
+      cycle interactive_loop
+    end if
+
+!   Compute area and volume.  The ** means "raise to a power."
+
+    area = pi * radius**2
+    volume=0.75*pi*radius**3
+
+!   Write the input variables (radius)
+!   and output (area and volume) to the screen.
+    write(*,*) 'The area and volume for the sphere with radius given, are:'
+    write (*,'(1x,a7,f6.2,5x,a7,f6.2,5x,a7,f6.2)') &
+      'radius=',radius,'area=',area,'volume=',volume
+
+    yn = ' '
+    yn_loop: do
+      write(*,*) 'Perform another calculation? y[n]'
+      read(*,'(a1)') yn
+      if (yn=='y' .or. yn=='Y') exit yn_loop
+      if (yn=='n' .or. yn=='N' .or. yn==' ') exit interactive_loop
+    end do yn_loop
+
+  end do interactive_loop
+
+end program sphere
